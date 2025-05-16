d5b955b91346bf1057446696f6a0548f60aa4871
ilker balcılar <ilkerbalcilartr@gmail.com>
2025-05-16 22:20:58 +0300

    runner

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ import {
 } from "instagram-graph-api";
 import { fileURLToPath } from "url";
 import { writeFile } from "fs/promises";
-import champions from "./champions.json" assert { type: "json" };
+import champions from './champions.json' with { type: "json" };
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
